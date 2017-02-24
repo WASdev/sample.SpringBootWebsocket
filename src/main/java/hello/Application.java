45746fa952713d7daad151c3a3042ec3449258e0
Charles Y. Tian <ctian@us.ibm.com>
2017-02-24 12:48:16 -0600

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,33 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletRegistration.Dynamic;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+        ctx.register(WebConfig.class);
+        ctx.setServletContext(servletContext);
+        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+        dynamic.addMapping("/");  
+        dynamic.setLoadOnStartup(1);
+        dynamic.setAsyncSupported(true);
+   }
+
+}
---
+++ src/main/java/hello/WebConfig.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan("hello")
+@Import({ WebSocketConfig.class })
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	  @Override
+	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		  registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	  }
+	
+} 
